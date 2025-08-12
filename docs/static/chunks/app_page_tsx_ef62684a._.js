f486699dec206950bc41650a2d257905b5e362f4
linknasubi <linknasubi2@gmail.com>
2025-08-12 09:58:37 -0300

    builded project

--- docs/static/chunks/app_page_tsx_ef62684a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_18054496._.js",
-  "static/chunks/node_modules_0eceb22d._.js"
-],
-    source: "dynamic"
-});
